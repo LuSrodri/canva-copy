ca74662ec7db0025a48d33fae4bb10517c11aa33
Lucas Santos Rodrigues <lucassrodri08@gmail.com>
2025-03-12 18:57:42 -0300

    refactor image handling and display logic for background removal tool

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const dropZone = document.getElementById('drop-zone');
 const addFilesButton = document.getElementById('add-files');
 const fileInput = document.getElementById('file-input');
 const filesToRemoveBackground = [];
+const divImages = document.getElementById('images');
+const showQtdImages = document.getElementById('show-qtd-images');
+const qtdImages = document.getElementById('qtd-images');
 
 addFilesButton.addEventListener('click', () => {
     fileInput.click();
@@ -34,6 +37,7 @@ fileInput.addEventListener('change', (e) => {
     const files = e.target.files;
     if (files.length) {
         handleFiles(files);
+        e.target.value = '';
     }
 });
 
@@ -44,13 +48,62 @@ function handleFiles(files) {
     if (allowedTypes.includes(file.type)) {
         filesToRemoveBackground.push(file);
         console.log("Arquivos válidos:", filesToRemoveBackground);
+        showQtdImages.style.display = 'flex';
+        qtdImages.textContent = filesToRemoveBackground.length;
+        showImage(file);
     } else {
         alert("Tipo de arquivo não permitido. Escolha JPG ou PNG.");
     }
 }
 
-const segmenter = await pipeline('background-removal', 'Xenova/modnet');
-const url = 'https://huggingface.co/datasets/Xenova/transformers.js-docs/resolve/main/portrait-of-woman_small.jpg';
-const output = await segmenter(url);
+function showImage(file) {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+        const divImage = document.createElement('div');
+        divImage.classList.add('image');
 
-console.log(output);
+        const img = document.createElement('img');
+        img.src = e.target.result;
+
+        const divShowImage = document.createElement('div');
+        divShowImage.classList.add('show-image');
+        divShowImage.appendChild(img);
+
+        divImage.appendChild(divShowImage);
+
+        const divButtons = document.createElement('div');
+        divButtons.classList.add('buttons');
+
+        const buttonToDelete = document.createElement('button');
+        buttonToDelete.classList.add('btn');
+        buttonToDelete.classList.add('remove-file');
+        buttonToDelete.textContent = 'Desistir';
+        buttonToDelete.addEventListener('click', () => {
+            removeImageFromArray(file);
+            divImage.remove();
+            img.remove();
+        });
+
+        divButtons.appendChild(buttonToDelete);
+
+        divImage.appendChild(divButtons);
+
+        divImages.appendChild(divImage);
+    };
+    reader.readAsDataURL(file);
+}
+
+function removeImageFromArray(file) {
+    const index = filesToRemoveBackground.indexOf(file);
+    if (index > -1) {
+        filesToRemoveBackground.splice(index, 1);
+    }
+    console.log("Arquivos válidos:", filesToRemoveBackground);
+    qtdImages.textContent = filesToRemoveBackground.length;
+}
+
+// const segmenter = await pipeline('background-removal', 'Xenova/modnet');
+// const url = 'https://huggingface.co/datasets/Xenova/transformers.js-docs/resolve/main/portrait-of-woman_small.jpg';
+// const output = await segmenter(url);
+
+// console.log(output);
